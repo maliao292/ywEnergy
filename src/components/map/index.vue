f25793aeeb083ed467d1873e2e9f1a52b15010d5
maliao292 <maming292@163.com>
2020-08-03 14:53:17 +0800

    map number

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -1,18 +1,103 @@
 <template>
   <div class='ywMap'>
+    <div class="mapMuneBtn">
+      <span class="arrow" :class="{menuActve:menushow}" @click="menushow=!menushow"></span>
+    </div>
     <div class="amap-page-container">
       <el-amap ref="map" vid="amapDemo" :amap-manager="amapManager" :zoom="zoom" :events="events" class="amap-demo">
       </el-amap>
     </div>
+    <transition name='fade'>
+      <div v-show='menushow' class="menuList">
+        <MapMenu />
+      </div>
+    </transition>
+
+    <div class="mapUseEleMsg">
+      <ul>
+        <li>
+          <p><b>530</b><span>个</span></p>
+          <span>基站总数</span>
+        </li>
+        <li>
+          <p><b>15600</b><span>kWh</span></p>
+          <span>本月总削峰电量</span>
+        </li>
+        <li>
+          <p><b>17500</b><span>kWh</span></p>
+          <span>本月总填谷电量</span>
+        </li>
+        <li>
+          <p><b>15600</b><span>元</span></p>
+          <span>本月总节省电费</span>
+        </li>
+        <li>
+          <p><b>800</b><span>kW</span></p>
+          <span>总可响应负荷</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="iconStatus">
+      <ul>
+        <li>
+          <div>
+            <img :src="iconImg.d" alt="">
+          </div>
+          <p>1</p>
+        </li>
+        <li>
+          <div>
+            <img :src="iconImg.c" alt="">
+          </div>
+          <p>1</p>
+        </li>
+        <li>
+          <div>
+            <img :src="iconImg.f" alt="">
+          </div>
+          <p>1</p>
+        </li>
+        <li>
+          <div>
+            <img :src="iconImg.t" alt="">
+          </div>
+          <p>1</p>
+        </li>
+        <li>
+          <div>
+            <img :src="iconImg.w" alt="">
+          </div>
+
+          <p>1</p>
+        </li>
+        <li>
+          <div>
+            <img :src="iconImg.l" alt="">
+          </div>
+          <p>1</p>
+        </li>
+      </ul>
+    </div>
   </div>
-</template>
+</template> 
 
 <script>
 import { amapManager, lazyAMapApiLoaderInstance } from 'vue-amap'
+import MapMenu from './MapMenu'
 export default {
-  components: {},
+  components: { MapMenu },
   data() {
     return {
+      iconImg: {
+        d: require('@/assets/img/d.png'),
+        c: require('@/assets/img/c.png'),
+        f: require('@/assets/img/f.png'),
+        t: require('@/assets/img/t.png'),
+        w: require('@/assets/img/w.png'),
+        l: require('@/assets/img/l.png'),
+      },
+      menushow: false,
       amapManager,
       zoom: 12,
       events: {
@@ -26,12 +111,12 @@ export default {
         moveend: () => {},
         zoomchange: () => {},
       },
-  
     }
   },
   created() {},
   mounted() {},
   methods: {
+    menuShowBtn() {},
     getMap() {
       // amap vue component
       console.log(amapManager._componentMap)
@@ -42,9 +127,42 @@ export default {
 }
 </script>
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  left: -250px;
+}
+
 .ywMap {
   position: relative;
   height: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+.ywMap .mapMuneBtn {
+  width: 50px;
+  flex-shrink: 0;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.ywMap .mapMuneBtn span {
+  display: inline-block;
+  margin-right: 10px;
+}
+.mapMuneBtn .arrow.menuActve {
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border-right: 10px solid transparent;
+  border-top: 10px solid transparent;
+  border-left: 10px solid #333;
+  border-bottom: 10px solid transparent;
+  margin-left: 10px;
+  margin-right: 0;
 }
 .ywMap .amap-page-container {
   width: 100%;
@@ -53,6 +171,15 @@ export default {
   left: 0;
   top: 0; */
   border: 1px solid #999;
-  z-index: 1111111111;
+  z-index: 10;
+}
+.menuList {
+  position: absolute;
+  top: 0;
+  left: 50px;
+  bottom: 0;
+  width: 200px;
+  z-index: 11;
+  background: #000710;
 }
 </style>
